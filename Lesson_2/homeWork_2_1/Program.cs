d8fbd9e52b2d1620b934f7656f6737f76f737558
Kate Zhirkova <kate.ololo@gmail.com>
2025-05-06 16:14:17 +0300

    Added Homework by Lesson 2

---
+++ Lesson_2/homeWork_2_1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace homeWork_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Write your age:");
+            int age;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out age) && age >= 0)
+                    break;
+                Console.WriteLine("Age isn't correct.");
+            }
+
+
+            double height;
+            while (true)
+            {
+                Console.WriteLine("Write your height:");
+                if (double.TryParse(Console.ReadLine(), out height))
+                    break;
+                Console.WriteLine("Height isn't correct.");
+            }
+
+            Console.WriteLine("***************************************");
+            Console.WriteLine($"Hi, {name}!\n\tYour age: {age}.\n\t\tYour height: {height}.");
+            Console.WriteLine("***************************************");
+        }
+    }
+}
