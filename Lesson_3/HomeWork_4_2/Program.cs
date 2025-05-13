d5713dac7bf5d5eaa2c671b06f18ba0dd440ef5d
Kate Zhirkova <kate.ololo@gmail.com>
2025-05-14 00:04:25 +0300

    Added Homework 4

---
+++ Lesson_3/HomeWork_4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeWork_4_2;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 10; i <= 99; ++i)
+            {
+                if(i%5 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
---
+++ Lesson_3/HomeWork_4_3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeWork_4_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var number = -20;
+
+        do
+        {
+            Console.WriteLine(number);
+            number -= 20;
+        } while (number >= -100);
+    }
+}
