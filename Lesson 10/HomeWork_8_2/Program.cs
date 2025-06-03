5e37c0cad75d14b43587f4cb3f3bff8139da4e4b
Kate Z <k.zgirkovaaa@gmail.com>
2025-06-03 18:52:13 +0300

    Added Homework 8

---
+++ Lesson 10/HomeWork_8_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace HomeWork_8_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? input;
+
+        do
+        {
+            Console.WriteLine("Enter a string:");
+            input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("The string mustn't be empty.\n");
+                continue;
+            }
+
+            break;
+        }while (true);
+
+        Console.WriteLine("String without spaces: " + RemoveSpaces(input));
+    }
+
+    static string RemoveSpaces(string input)
+    {
+        var sb = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if(c != ' ')
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
